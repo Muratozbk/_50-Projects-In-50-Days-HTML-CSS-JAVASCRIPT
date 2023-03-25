7b6e1773a8d6bb1fdc26973fabb8edca0dbaa9b8
murat <slipstream11@yandex.com>
2023-03-25 20:22:54 +0300

    ratating nav js

--- Rotating Navigation 3/script.js
+++ Rotating Navigation 3/script.js
@@ -1,1 +1,11 @@
+'use strict';
+const container = document.querySelector('.container');
+const openBtn = document.getElementById('open');
+const closeBtn = document.getElementById('close');
 
+openBtn.addEventListener('click', () => {
+    container.classList.add('show-nav');
+})
+closeBtn.addEventListener('click', () => {
+    container.classList.remove('show-nav');
+})
