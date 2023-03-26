c0f03535a27aa46eb57a9825df5cfb7519b35863
murat <slipstream11@yandex.com>
2023-03-26 20:14:15 +0300

    split landing page

--- 6 Scroll Animation/script.js
+++ 6 Scroll Animation/script.js
@@ -4,11 +4,12 @@ window.addEventListener('scroll', checkBoxes);
 
 checkBoxes()
 function checkBoxes() {
+    console.log(window.innerHeight)
     const trigerBottom = window.innerHeight / 5 * 4;
 
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top;
-
+        // console.log(boxTop)
         if (boxTop < trigerBottom) {
             box.classList.add('show')
         } else {
@@ -16,3 +17,4 @@ function checkBoxes() {
         }
     })
 }
+
