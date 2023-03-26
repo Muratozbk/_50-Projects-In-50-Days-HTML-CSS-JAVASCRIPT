f6af0c90bba8588244c437ce25450f6ccaad6b5d
murat <slipstream11@yandex.com>
2023-03-26 12:59:26 +0300

    blurry loading

---
+++ 5 Blurry Loading/script.js
@@ -0,0 +1,39 @@
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+let int = setInterval(blurring, 30)
+
+function blurring() {
+    load++;
+
+    if (load > 99) {
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    // loadText.style.opacity = `${1 - (load / 100)}`
+
+    // bg.style.filter = `blur(${30 - (30 * (load / 100))}px)`
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    // console.log(load)
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) /
+        (in_max - in_min) + out_min
+}
+
+
+let loat2 = 0;
+
+let interval = setInterval(blurring2, 30)
+
+function blurring2() {
+    loat2++;
+    if (loat2 > 29) {
+        clearInterval(interval)
+    }
+    console.log(loat2)
+}
