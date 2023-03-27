10d0288461b40618d4f32ae0a741812ee32cea6d
murat <slipstream11@yandex.com>
2023-03-27 12:04:40 +0300

    form input wave js

---
+++ 8 Form Input Login Wave/script.js
@@ -0,0 +1,18 @@
+const labels = document.querySelectorAll('.form-control label');
+
+// labels.forEach(label => {
+//     label.innerHTML = label.innerText
+//         .split('')
+//         .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+//         .join('')
+// })
+
+// < label ><span>E</span><span>m</span><span>a</span><span>i</span><span>l</span></label >
+
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, i) => ` <span style="transition-delay:${i * 44}ms">${letter}</span>`)
+        .join('')
+})
