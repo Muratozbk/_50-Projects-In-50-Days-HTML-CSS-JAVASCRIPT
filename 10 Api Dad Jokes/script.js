7512b76a13128b4a8e9efa78ba24bd2a535708a5
murat <slipstream11@yandex.com>
2023-03-27 18:16:46 +0300

    fetch api

---
+++ 10 Api Dad Jokes/script.js
@@ -0,0 +1,39 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+// const userAction = async () => {
+//     const response = await fetch('http://example.com/movies.json');
+//     const myJson = await response.json();
+//     console.log(myJson)
+// }
+// userAction()
+
+generateJoke()
+
+//// Async Await
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: "application/json",
+            // "Content-Type": "application/json",
+        },
+    }
+    const res = await fetch('https://icanhazdadjoke.com/', config)
+    const data = await res.json()
+    jokeEl.innerHTML = data.joke
+}
+
+//// Using .then()
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept: 'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then(res => res.json())
+//         // .then(data => console.log(data))
+//         .then(data => jokeEl.innerHTML = data.joke)
+// }
