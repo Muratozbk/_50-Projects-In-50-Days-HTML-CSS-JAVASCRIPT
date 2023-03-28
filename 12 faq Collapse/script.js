f9611e7229c10508f42a5b62542a947917467cec
murat <slipstream11@yandex.com>
2023-03-28 12:09:03 +0300

    faq questions js

---
+++ 12 faq Collapse/script.js
@@ -0,0 +1,23 @@
+/*
+1-Loop through nodelist (forEach)
+2-add click event
+3-Toggle the active class on the parent node 
+(.parentNode& classList.toggle())
+*/
+
+//ParentNode: tek kendi bölümünü seçer, SelectAll hepsini seçer
+//closest('div') gibi
+
+const toggleBtns = document.querySelectorAll('.faq-toggle')
+const boxes = document.querySelectorAll('.faq')
+
+toggleBtns.forEach(toggle => {
+    toggle.addEventListener('click', () =>
+        toggle.parentNode.classList.toggle('active')
+    )
+})
+
+
+// toggleBtns.forEach(btn =>
+//     btn.addEventListener('click', () =>
+//         btn.closest('div').classList.toggle('active')))
