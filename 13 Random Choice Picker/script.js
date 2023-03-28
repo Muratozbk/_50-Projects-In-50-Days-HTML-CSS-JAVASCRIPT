e561b9206bab5a58e342c2f391de78a4ee67f62d
murat <slipstream11@yandex.com>
2023-03-28 18:37:31 +0300

    Random Choice js

---
+++ 13 Random Choice Picker/script.js
@@ -0,0 +1,68 @@
+const textarea = document.getElementById('textarea')
+const tagsEl = document.getElementById('tags');
+
+// tagsEl.innerHTML = `<span class="tag">Choice 3</span>`
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+
+    if (e.key === 'Enter') {
+        setTimeout(() => e.target.value = ''), 10
+        randomSelect()
+    }
+})
+
+function createTags(input) {
+    console.log(input)
+
+    const tags = input.split(',').filter(tag => tag.trim()
+        !== '').map(tag => tag.trim())
+
+    console.log(tags)
+
+    tagsEl.innerHTML = '';
+
+    tags.forEach(tagEach => {
+        const tagElEach = document.createElement('span')
+        tagElEach.classList.add('tag')
+        tagElEach.innerHTML = tagEach
+        tagsEl.append(tagElEach)
+    })
+
+}
+
+function randomSelect() {
+    const times = 30; // Kaç defa hareket edicek
+
+    const interval = setInterval(() => {
+
+        const randomTag = pickRandomTag()
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+    }, 100)
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag(); // 1 tane seçili kalır
+            highlightTag(randomTag)
+        }, 100)
+    }, times * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
+}
