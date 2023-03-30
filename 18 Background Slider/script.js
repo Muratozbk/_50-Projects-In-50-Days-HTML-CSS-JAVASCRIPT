e14777a804a559a7b399e4aa52f64525f48c03f8
murat <slipstream11@yandex.com>
2023-03-30 21:38:05 +0300

    changing slides

---
+++ 18 Background Slider/script.js
@@ -0,0 +1,31 @@
+const slides = document.querySelectorAll('.slide')
+const right = document.getElementById('right')
+const left = document.getElementById('left')
+const body = document.body;
+
+let activeSlide = 0;
+setBgToBody()
+
+right.addEventListener('click', () => {
+    activeSlide++
+    if (activeSlide > slides.length - 1) activeSlide = 0
+    setBgToBody()
+    setActiveSlide()
+})
+left.addEventListener('click', () => {
+    activeSlide--
+    if (activeSlide < 0) activeSlide = slides.length - 1
+    setBgToBody()
+    setActiveSlide()
+})
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlide() {
+    slides.forEach(slide =>
+        slide.classList.remove('active'))
+
+    slides[activeSlide].classList.add('active')
+}
