148457ba45e93f030afaaea8b5fec4006cbe7f6d
murat <slipstream11@yandex.com>
2023-03-31 20:48:06 +0300

    drag n drop

--- 20 Button Ripple Effect/script.js
+++ 20 Button Ripple Effect/script.js
@@ -1,41 +1,25 @@
 const buttons = document.querySelectorAll('.ripple')
 
-// buttons.forEach(button => {
-//     button.addEventListener('click', function (e) {
-//         const x = e.clientX;
-//         const y = e.clientY;
-
-//         const buttonTop = e.target.offsetTop
-//         const buttonLeft = e.target.offsetLeft
-
-//         const xInside = x - buttonLeft
-//         const yInside = y - buttonTop;
-//         // console.log(y, x)
-//         console.log(xInside, yInside)
-
-//         const circle = document.createElement('span')
-//         circle.classList.add('circle');
-//         circle.style = `top:${yInside}px; left:${xInside}px`
-
-//         this.appendChild(circle) //arrow fonskiyonda this çalışmaz
-
-//         setTimeout(() => circle.remove(), 1000) // for clean dom
-//     })
-// })
-
 buttons.forEach(button => {
     button.addEventListener('click', function (e) {
         const x = e.clientX;
-        const buttonLeft = e.target.offsetLeft;
-        const xInside = x - buttonLeft;
         const y = e.clientY;
-        const buttonTop = e.target.offsetTop;
-        const yInside = y - buttonTop
-        console.log(yInside)
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop;
+        // console.log(y, x)
+        console.log(xInside, yInside)
 
         const circle = document.createElement('span')
         circle.classList.add('circle');
-        circle.style = `top:${yInside}px; left:${xInside}px `;
-        this.appendChild(circle)
+        circle.style = `top:${yInside}px; left:${xInside}px`
+
+        this.appendChild(circle) //arrow fonskiyonda this çalışmaz
+
+        setTimeout(() => circle.remove(), 1000) // for clean dom
     })
 })
+
---
+++ 21 Drag n Drop/script.js
@@ -0,0 +1,42 @@
+// fill hold hovered empty
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+for (const empty of empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+function dragStart() {
+    // this.classList.add('hold')
+    this.className += ' hold'
+    setTimeout(() => this.className = 'invisible', 0)
+    console.log('drag start')
+}
+function dragEnd() {
+    this.className = 'fill'
+    console.log('dragEnd')
+}
+function dragOver(e) {
+    e.preventDefault()
+    console.log('dragOver')
+}
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
+    console.log('dragEnter')
+}
+function dragLeave() {
+    console.log('dragLeave')
+    this.className = 'empty'
+}
+function dragDrop() {
+    this.className = 'empty';
+    this.append(fill)
+    console.log('drag Drop')
+}
