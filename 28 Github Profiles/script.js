11247909328d1b8787572475c51a0c826b45990d
murat <slipstream11@yandex.com>
2023-04-04 20:15:45 +0300

    fetch profile data

---
+++ 28 Github Profiles/script.js
@@ -0,0 +1,25 @@
+const API_URL = 'https://api.github.com/users/'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+//// getUser('bradtraversy')
+
+async function getUser(username) {
+    try {
+        const { data } = await axios(API_URL + username)
+
+        console.log(data)
+    } catch (err) { console.log(err) }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+    if (user) {
+        getUser(user)
+
+        search.value = '';
+    }
+})
