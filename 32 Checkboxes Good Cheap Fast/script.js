45190b96e472deaab5d9c6219b8f369f1770c5a1
murat <slipstream11@yandex.com>
2023-04-06 18:45:35 +0300

    note app

--- 32 Checkboxes Good Cheap Fast/script.js
+++ 32 Checkboxes Good Cheap Fast/script.js
@@ -1,4 +1,30 @@
+const toggles = document.querySelectorAll('.toggle')
 const good = document.getElementById('good')
 const cheap = document.getElementById('cheap')
 const fast = document.getElementById('fast')
+
+toggles.forEach(toggle =>
+    toggle.addEventListener('change', (e) => doTheTrick(e.target)))
+
+function doTheTrick(clickedOne) {
+    if (good.checked && cheap.checked && fast.checked) {
+        if (good === clickedOne) {
+            fast.checked = false
+        }
+        if (cheap === clickedOne) {
+            good.checked = false
+        }
+        if (fast === clickedOne) {
+            cheap.checked = false
+        }
+    }
+}
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => {
+    if (good.checked && cheap.checked && fast.checked) {
+        if (e.target === good) cheap.checked = false;
+        if (e.target === cheap) fast.checked = false;
+        if (e.target === fast) good.checked = false;
+    }
+}))
 
