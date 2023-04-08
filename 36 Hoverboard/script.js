68f03e9a35460c1b548d38e9808f956dadffab2c
murat <slipstream11@yandex.com>
2023-04-08 12:04:51 +0300

    update data index

--- 36 Hoverboard/script.js
+++ 36 Hoverboard/script.js
@@ -3,18 +3,6 @@ const container = document.getElementById('container')
 const allColors = document.getElementById('allColors')
 const colorPicker = document.getElementById('color')
 
-colorPicker.addEventListener('change', () => {
-    const colorPicked = colorPicker.value;
-    console.log(colorPicked)
-    colors.push(colorPicked)
-    // colors.shift()
-    const square = document.createElement('div')
-    square.classList.add('square')
-    square.classList.add('sq22')
-    square.style.backgroundColor = colorPicked
-    allColors.appendChild(square)
-})
-
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', 'rgb(91, 209, 98)']
 
 const SQUARES = 500;
@@ -24,7 +12,6 @@ for (let i = 0; i < SQUARES; i++) {
     square.classList.add('square');
 
     square.addEventListener('mouseover', () => setColor(square));
-
     square.addEventListener('mouseout', () => removeColor(square));
 
     container.appendChild(square)
@@ -55,17 +42,31 @@ for (let i = 0; i < colors.length; i++) {
     square.classList.add('square')
     square.style.backgroundColor = colors[i]
     allColors.appendChild(square);
+    square.setAttribute('data-index', i); // Add a data-index attribute to each square
 }
 
+allColors.addEventListener('click', function (e) {
+    const square = e.target;
+    if (square.classList.contains('square')) {
+        const index = square.getAttribute('data-index'); // Get the index of the color
+        square.remove(); // Remove the square from the DOM
+        colors.splice(index, 1); // Remove the color from the colors array
 
-allColors.querySelectorAll('.square').forEach((sqr, idx) => {
-    sqr.addEventListener('click', function (e) {
-        e.target.remove()
-        colors.splice(idx, 1)
-    })
+        allColors.querySelectorAll('.square').forEach((sqr, idx) => {
+            sqr.setAttribute('data-index', idx); // Update the data-index of the remaining squares
+        });
+    }
+});
+
+colorPicker.addEventListener('change', () => {
+    const colorPicked = colorPicker.value;
+    console.log(colorPicked)
+    colors.push(colorPicked)
+    // colors.shift()
+    const square = document.createElement('div')
+    square.classList.add('square')
+    square.style.backgroundColor = colorPicked
+    allColors.appendChild(square)
+    square.setAttribute('data-index', colors.length - 1); // Add a data-index attribute to the new square
 })
-
-
-
-
 
