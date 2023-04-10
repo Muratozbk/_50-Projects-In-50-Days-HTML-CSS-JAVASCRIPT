9f1ef20b80da9cbe19e0db03d950c4c7a8c282d8
murat <slipstream11@yandex.com>
2023-04-10 19:50:13 +0300

    feedback UI

--- 42 Live User Filter/script.js
+++ 42 Live User Filter/script.js
@@ -1,36 +1,88 @@
 const result = document.getElementById('result')
-const filter = document.getElementById('filter')
-const listIttems = [];
+const filterText = document.getElementById('filter')
+filterText.focus()
+
+// const listIttems = [];
+
+// getData()
+
+// filterText.addEventListener('input', (e) => filterData(e.target.value))
+
+// async function getData() {
+//     const res = await fetch('https://randomuser.me/api?results=50')
+//     const { results } = await res.json() // data.results
+
+//     result.innerHTML = ''
+
+//     results.forEach(user => {
+//         const li = document.createElement('li')
+
+//         listIttems.push(li)
+
+//         li.innerHTML = `
+//        <img src="${user.picture.medium}" alt="${user.name.first}">
+//        <div class="user-info">
+//        <h4>${user.name.first} ${user.name.last}
+//        <p>${user.location.city}, ${user.location.country}
+//        `
+//         result.appendChild(li)
+//     })
+// }
+
+// function filterData(searchTerm) {
+//     listIttems.forEach(item => {
+//         if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+//             item.classList.remove('hide')
+//         } else { item.classList.add('hide') }
+//     })
+// }
+
+const listItems = []
+
+filterText.addEventListener('input', (e) => filterData(e.target.value))
 
 getData()
-
-filter.addEventListener('input', (e) => filterData(e.target.value))
-
 async function getData() {
-    const res = await fetch('https://randomuser.me/api?results=50')
-    const { results } = await res.json() // data.results
+    const res = await fetch('https://randomuser.me/api?results=100')
+    const { results } = await res.json()
+    const excludeCountries = ['Iran', 'Turkey']
+    console.log(results.filter(data => data.gender === 'female'))
+    const females = results.filter(data => (data.gender === 'female' &&
+        !excludeCountries.includes(data.location.country))) //Female users filtered
 
     result.innerHTML = ''
 
-    results.forEach(user => {
+    females.forEach(user => {
         const li = document.createElement('li')
-
-        listIttems.push(li)
-
         li.innerHTML = `
-       <img src="${user.picture.medium}" alt="${user.name.first}">
-       <div class="user-info">
-       <h4>${user.name.first} ${user.name.last}
-       <p>${user.location.city}, ${user.location.country}
-       `
+    <img src="${user.picture.large}" alt="${user.name.first}">
+    <div class="user-info">
+      <h4>${user.name.first} ${user.name.last}</h4>
+      <p>${user.location.city}, ${user.location.country}</p>
+    </div>`
+        listItems.push(li)
         result.appendChild(li)
     })
+    console.log(listItems)
+
 }
 
 function filterData(searchTerm) {
-    listIttems.forEach(item => {
+    listItems.forEach(item => {
         if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
             item.classList.remove('hide')
-        } else { item.classList.add('hide') }
+        } else {
+            item.classList.add('hide');
+        }
     })
+    console.log(listItems)
 }
+
+// Filer female users 
+// const females = results.filter(data => (data.gender === 'female' && data.location.country !== 'Turkey' && data.location.country !== 'Iran')) //Female users filtered
+
+//Better filter method
+const results2 = ['a', 'b']
+const excludeCountries2 = ['Iran', 'Turkey']
+const females2 = results2.filter(data => (data.gender === 'female' &&
+    !excludeCountries2.includes(data.location.country))) //pick female and not pick this countries
---
+++ 43 Feedback UI Design/script.js
@@ -0,0 +1,32 @@
+const sendBtn = document.getElementById('send')
+const panel = document.getElementById('panel')
+const ratingsContainer = document.querySelector('.ratings-container')
+const ratings = document.querySelectorAll('.rating')
+
+let selectedRating = 'Satisfied'
+
+ratingsContainer.addEventListener('click', (e) => {
+    if (e.target.parentNode.classList.contains('rating')) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+
+        selectedRating = e.target.closest('div').querySelector('small').innerHTML
+
+    }
+})
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+    <i class="fa fa-heart"></i>
+<strong>Thank you</strong>
+<br>
+<strong>Feedback: ${selectedRating}</strong>
+<p>We'll use your feedback to improve our customer support</p>`
+})
+
+function removeActive() {
+    // ratings.forEach(rating => rating.classList.remove('active'))
+    for (let i = 0; i < ratings.length; i++) {
+        ratings[i].classList.remove('active')
+    }
+}
