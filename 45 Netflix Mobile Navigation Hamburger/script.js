7a34776120d52d070efc9795b2657306a706be32
murat <slipstream11@yandex.com>
2023-04-11 13:54:48 +0300

    open close nav

--- 45 Netflix Mobile Navigation Hamburger/script.js
+++ 45 Netflix Mobile Navigation Hamburger/script.js
@@ -1,1 +1,19 @@
+const navs = document.querySelectorAll('.nav')
+const btnOpen = document.querySelector('.open-btn')
+const btnClose = document.querySelector('.close-btn')
 
+btnOpen.addEventListener('click', () => {
+    navs.forEach((nav, idx) => {
+        nav.classList.add('visible')
+    })
+})
+
+btnClose.addEventListener('click', () => {
+    navs.forEach((nav, idx) => {
+        setTimeout(() => nav.classList.remove('visible'), idx * 1)
+
+    })
+})
+
+
+navs.forEach(nav => nav.classList.remove('visible'))
