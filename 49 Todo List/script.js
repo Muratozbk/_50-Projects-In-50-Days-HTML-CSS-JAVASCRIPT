db0262040e75f7b10c2d0099c005f91181c0a8eb
murat <slipstream11@yandex.com>
2023-04-13 12:12:45 +0300

    todo list js

--- 49 Todo List/script.js
+++ 49 Todo List/script.js
@@ -2,7 +2,68 @@ const todosUL = document.getElementById('todos')
 const form = document.getElementById('form')
 const input = document.getElementById('input')
 
-const todos = JSON.parse(localStorage.getItem('todos'));
+// const todos = JSON.parse(localStorage.getItem('todos'));
+
+// if (todos) {
+//     todos.forEach(todo => addTodo(todo))
+// }
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+
+//     addTodo()
+// })
+
+
+// function addTodo(todo) {
+//     let todoText = input.value;
+
+//     if (todo) {
+//         todoText = todo.text
+//         console.log(todo)
+//     }
+//     if (todoText) {
+//         const todoEl = document.createElement('li')
+//         if (todo && todo.completed) {
+//             todoEl.classList.add('completed')
+//         }
+//         todoEl.innerText = todoText;
+
+//         todoEl.addEventListener('click', () => {
+//             todoEl.classList.toggle('completed');
+//             updateLS()
+//         })
+
+//         todoEl.addEventListener('contextmenu', (e) => {
+//             e.preventDefault();
+//             todoEl.remove()
+//             updateLS()
+//         })
+
+//         todosUL.appendChild(todoEl);
+
+//         input.value = '';
+
+//         updateLS()
+//     }
+// }
+
+// function updateLS() {
+//     const todosEl = document.querySelectorAll('li')
+//     const todos = []
+
+//     todosEl.forEach(todoEl => {
+//         todos.push({
+//             text: todoEl.innerText,
+//             completed: todoEl.classList.contains('completed') //it gives true or false for completed classlist
+//         })
+//     })
+//     console.log(todos)
+//     localStorage.setItem('todos', JSON.stringify(todos))
+// }
+
+const todos = JSON.parse(localStorage.getItem('todos'))
+console.log(todos)
 
 if (todos) {
     todos.forEach(todo => addTodo(todo))
@@ -10,14 +71,11 @@ if (todos) {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-
     addTodo()
 })
 
-
 function addTodo(todo) {
-    let todoText = input.value;
-
+    let todoText = input.value
     if (todo) {
         todoText = todo.text
     }
@@ -26,35 +84,32 @@ function addTodo(todo) {
         if (todo && todo.completed) {
             todoEl.classList.add('completed')
         }
-        todoEl.innerText = todoText;
-
-        todoEl.addEventListener('click', () => {
-            todoEl.classList.toggle('completed');
-            updateLS()
-        })
+        todoEl.innerText = todoText
 
         todoEl.addEventListener('contextmenu', (e) => {
-            e.preventDefault();
+            e.preventDefault()
             todoEl.remove()
+            updateLS()
+        })
+        todoEl.addEventListener('click', (e) => {
+            e.preventDefault()
+            todoEl.classList.toggle('completed')
             updateLS()
         })
 
         todosUL.appendChild(todoEl);
-
         input.value = '';
-
         updateLS()
     }
 }
 
 function updateLS() {
-    const todosEl = document.querySelectorAll('li')
     const todos = []
-
+    const todosEl = document.querySelectorAll('li')
     todosEl.forEach(todoEl => {
         todos.push({
             text: todoEl.innerText,
-            completed: todoEl.classList.contains('completed') //itt gives true or false for completed classlist
+            completed: todoEl.classList.contains('completed')
         })
     })
     console.log(todos)
@@ -63,20 +118,7 @@ function updateLS() {
 
 
 
-// const todoList = [];
 
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault()
-//     console.log(input.value)
-//     todoList.push(input.value)
-
-//     const todoEl = document.createElement('li')
-//     todoEl.innerText = input.value;
-//     console.log(todoList, todoEl)
-//     todosUL.appendChild(todoEl)
-//     input.value = '';
-// })
-// const todoEl = input.value
 
 // todosUL.addEventListener('contextmenu', (e) => {
 //     e.preventDefault()
