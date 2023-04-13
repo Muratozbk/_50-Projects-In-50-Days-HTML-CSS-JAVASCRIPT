ebbb39a6b8651a9929de077bf5ace2f737b92bc9
murat <slipstream11@yandex.com>
2023-04-13 14:28:02 +0300

    insect game css

--- 49 Todo List/script.js
+++ 49 Todo List/script.js
@@ -2,68 +2,7 @@ const todosUL = document.getElementById('todos')
 const form = document.getElementById('form')
 const input = document.getElementById('input')
 
-// const todos = JSON.parse(localStorage.getItem('todos'));
-
-// if (todos) {
-//     todos.forEach(todo => addTodo(todo))
-// }
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault()
-
-//     addTodo()
-// })
-
-
-// function addTodo(todo) {
-//     let todoText = input.value;
-
-//     if (todo) {
-//         todoText = todo.text
-//         console.log(todo)
-//     }
-//     if (todoText) {
-//         const todoEl = document.createElement('li')
-//         if (todo && todo.completed) {
-//             todoEl.classList.add('completed')
-//         }
-//         todoEl.innerText = todoText;
-
-//         todoEl.addEventListener('click', () => {
-//             todoEl.classList.toggle('completed');
-//             updateLS()
-//         })
-
-//         todoEl.addEventListener('contextmenu', (e) => {
-//             e.preventDefault();
-//             todoEl.remove()
-//             updateLS()
-//         })
-
-//         todosUL.appendChild(todoEl);
-
-//         input.value = '';
-
-//         updateLS()
-//     }
-// }
-
-// function updateLS() {
-//     const todosEl = document.querySelectorAll('li')
-//     const todos = []
-
-//     todosEl.forEach(todoEl => {
-//         todos.push({
-//             text: todoEl.innerText,
-//             completed: todoEl.classList.contains('completed') //it gives true or false for completed classlist
-//         })
-//     })
-//     console.log(todos)
-//     localStorage.setItem('todos', JSON.stringify(todos))
-// }
-
-const todos = JSON.parse(localStorage.getItem('todos'))
-console.log(todos)
+const todos = JSON.parse(localStorage.getItem('todos'));
 
 if (todos) {
     todos.forEach(todo => addTodo(todo))
@@ -71,50 +10,59 @@ if (todos) {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
+
     addTodo()
 })
 
+
 function addTodo(todo) {
-    let todoText = input.value
+    let todoText = input.value;
+
     if (todo) {
         todoText = todo.text
+        console.log(todo)
     }
-    if (todoText) {
+    if (todoText && todoText.trim() !== '') {
         const todoEl = document.createElement('li')
         if (todo && todo.completed) {
             todoEl.classList.add('completed')
         }
-        todoEl.innerText = todoText
+        todoEl.innerText = todoText;
 
-        todoEl.addEventListener('contextmenu', (e) => {
-            e.preventDefault()
-            todoEl.remove()
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed');
             updateLS()
         })
-        todoEl.addEventListener('click', (e) => {
-            e.preventDefault()
-            todoEl.classList.toggle('completed')
+
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+            todoEl.remove()
             updateLS()
         })
 
         todosUL.appendChild(todoEl);
+
         input.value = '';
+
         updateLS()
     }
 }
 
 function updateLS() {
-    const todos = []
     const todosEl = document.querySelectorAll('li')
+    const todos = []
+
     todosEl.forEach(todoEl => {
         todos.push({
             text: todoEl.innerText,
-            completed: todoEl.classList.contains('completed')
+            completed: todoEl.classList.contains('completed') //it gives true or false for completed classlist
         })
     })
     console.log(todos)
     localStorage.setItem('todos', JSON.stringify(todos))
 }
+
+
 
 
 
